125bf109bc4c2a3f6760ba41b1881f622671ba83
NANDO100888 <fernandocrdev@gmail.com>
2026-02-24 22:29:44 -0400

    config: preparar para despliegue en GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Desactivamos la optimización de imágenes nativa porque GH Pages no tiene servidor para procesarlas
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
